3c5d230a9b89c5c5989918fc62612bf17e6ffada
Rodrigo <rodrigovieiranunes23@outlook.com>
2021-02-17 09:35:00 -0300

    configuration composer and added class Search

---
+++ src/Search.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rodrigo\php_one;
+
+class Search{
+    private $url = "https://viacep.com.br/ws/";
+    
+    public function getAddressFromZipcode(string $zipCode):array{
+        $zipCode = preg_replace('/[^0-9]/im','',$zipCode);
+        $get = file_get_contents($this->url.$zipCode."/json");
+    }
+}
